d2d7fbae705bcd7ff844f717de9ce45be7c14312
Denis <den.listapad@mail.ru>
2023-10-11 21:56:36 +0300

    add models Products

--- shop/ecoshop/models.py
+++ shop/ecoshop/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Product(models.Model):
+    CATEGORY = [
+        ("FR", "Fruit"),
+        ("VG", "Vegetable"),
+        ("ML", "Milk products"),
+        ("MT", "Meat"),
+        ("TC", "Tea and coffee"),
+        ("FS", "Fish"),
+        ("AL", "Alcohol"),
+    ]
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100,blank=False,default="perfect products")
+    price = models.FloatField()
+    vendor = models.CharField(max_length=30)
+    amount = models.PositiveIntegerField()
+    delivery_date = models.DateField(auto_now_add=True)
+    category = models.CharField(
+        max_length=2,
+        choices=CATEGORY,
+    )
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
