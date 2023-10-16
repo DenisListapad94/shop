956b80c97b7dc08023b49b0ce645c230765bb7ef
Denis <den.listapad@mail.ru>
2023-10-16 23:40:56 +0300

    add postgresql and tables for shop

--- shop/ecoshop/admin.py
+++ shop/ecoshop/admin.py
@@ -1,3 +1,10 @@
+from .models import Product,ReviewsProducts,Vendor,Shipper,PassportShipper
+
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ReviewsProducts)
+admin.site.register(Vendor)
+admin.site.register(Shipper)
+admin.site.register(PassportShipper)
--- shop/ecoshop/models.py
+++ shop/ecoshop/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 
+
 class Product(models.Model):
     CATEGORY = [
         ("FR", "Fruit"),
@@ -15,7 +16,6 @@ class Product(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100,blank=False,default="perfect products")
     price = models.FloatField()
-    vendor = models.CharField(max_length=30)
     amount = models.PositiveIntegerField()
     delivery_date = models.DateField(auto_now_add=True)
     category = models.CharField(
@@ -24,6 +24,45 @@ class Product(models.Model):
     )
     def __str__(self):
         return self.name
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=30,null=True)
+    email = models.EmailField(null=True)
+    phone= models.CharField(max_length=30,null=True)
+    inn = models.PositiveIntegerField()
+    product = models.ManyToManyField("Product")
+
+    def __str__(self):
+        return self.name
+
+class Shipper(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=30,null=True)
+    email = models.EmailField(null=True)
+    phone = models.CharField(max_length=30,null=True)
+    product = models.ManyToManyField("Product")
+
+    def __str__(self):
+        return self.name
+
+class ReviewsProducts(models.Model):
+    title = models.CharField(max_length=30)
+    description= models.CharField(max_length=300,default="default review")
+    author = models.CharField(max_length=30)
+    reviews_time =  models.DateField(auto_now_add=True)
+    product = models.ForeignKey("Product",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class PassportShipper(models.Model):
+    passport_number = models.PositiveIntegerField()
+    passport_series = models.CharField(max_length=5)
+    shipper = models.OneToOneField("Shipper",on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.passport_number)
 
 
 
